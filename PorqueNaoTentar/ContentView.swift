be078378da511bf2bfcf4bfea01c61100b2407b6
milenabeicker <milenakessia87@gmail.com>
2025-06-17 11:03:57 -0300

    Initial Commit

---
+++ PorqueNaoTentar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PorqueNaoTentar
+//
+//  Created by Milena Beicker on 17/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PorqueNaoTentar/PorqueNaoTentarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PorqueNaoTentarApp.swift
+//  PorqueNaoTentar
+//
+//  Created by Milena Beicker on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PorqueNaoTentarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
