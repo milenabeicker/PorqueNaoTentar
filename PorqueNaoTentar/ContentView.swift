aef1752c05e7d4667832b1faa0e87f5d6ec1a1ee
milenabeicker <milenakessia87@gmail.com>
2025-06-18 11:14:11 -0300

    Abordei  conceitos básicos de SwiftUI que fui aprendendo: - exibição de texto e imagens - uso de botões com ação - organização com pilhas (VStack, HStack, ZStack) - introdução à animação - uso da propriedade @State para rastrear valores mutáveis ao longo do tempo

--- PorqueNaoTentar/ContentView.swift
+++ PorqueNaoTentar/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var atividades = ["Volleyball","Boxing", "Yoga", "Walk", "Dance", "Run", "Basketball","Flexibility", "Handball", "Jumprope", "Pilates"]
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+  
+    @State private var selected = "Volleyball"
+    @State private var id = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          //  Spacer()
+            
+            Text("Porque não tentar?")
+                .font(.largeTitle.bold())
+            
+            Spacer()
+            
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())").font(.system(size: 144))
+                            .foregroundColor(.white)
+                    )
+                
+                Text("\(selected)")
+                    .font(.title)
+            }
+            .transition(.slide)
+            .id(id)
+            
+            Spacer()
+            
+            Button("Tente novamente") {
+                // mudança de atividade quando o botão for pressionado
+                withAnimation(.easeInOut(duration: 1))  {
+                    selected = atividades
+                        .randomElement() ?? "Volleyball"; id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
+        
     }
 }
 
